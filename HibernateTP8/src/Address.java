5366d186c9771469fd2ca79f6b9786f6880e645d
Axel RANDOLPH <randolph.axel@yahoo.fr>
2019-12-19 09:50:14 +0100

    Initialisation Hibernate TP 8

---
+++ HibernateTP8/src/Address.java
@@ -0,0 +1,15 @@
+import javax.persistence.*;
+
+@Entity
+public class Address {
+	
+	private String street;
+	
+	private String city;
+	
+	private String State;
+	
+	private int postalCode;
+	
+	private String country;
+}
